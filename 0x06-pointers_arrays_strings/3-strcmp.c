73d330efce504887e8bfc78c37666719426f3b4a
jonero82 <jonero82@gmail.com>
2022-07-14 04:35:10 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
 * @s2: second string to compare
 *
 * Return: less than 0 if s1 is less than s2, 0 if they're equal,
-* more than 0 if s1 is greater than s2        
+* more than 0 if s1 is greater than s2
 */
 int _strcmp(char *s1, char *s2)
 {
@@ -15,7 +15,7 @@ while (*s1 == *s2)
 if (*s1 == '\0')
 {
 return (0);
-}							
+}
 s1++;
 s2++;
 }
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the new 0x06-pointers arrays strings
